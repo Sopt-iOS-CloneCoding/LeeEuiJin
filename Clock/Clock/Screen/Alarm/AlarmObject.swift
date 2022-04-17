fbf6c77102a8698b92df0d228e3489593857e2ac
lee-euijin <leeuj9663@naver.com>
2022-04-17 17:48:38 +0900

    #1 ✨[FEAT] : 알람화면 레이아웃 구성 (4) - 테이블 뷰 오토 레이아웃 제외 레이아웃 완성

--- Clock/Clock/Screen/Alarm/AlarmObject.swift
+++ Clock/Clock/Screen/Alarm/AlarmObject.swift
@@ -1,8 +1,15 @@
 //
-//  File.swift
+//  AlarmObjcet.swift
 //  Clock
 //
 //  Created by 이의진 on 2022/04/17.
 //
 
 import Foundation
+
+struct AlarmClock {
+    var alarmName : String
+    var alarmTime : String
+    var sound : String
+    var isSnooze : Bool
+}
--- Clock/Clock/Screen/Alarm/AlarmViewController.swift
+++ Clock/Clock/Screen/Alarm/AlarmViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 class AlarmViewController: UIViewController {
     
     @IBOutlet weak var alarmTableView: UITableView!
-    var tableViewItems = ["item1", "item2", "item3"]
+    var tableViewItems = [AlarmClock(alarmName: "알람 1", alarmTime: "08:30", sound: "TOMBOY", isSnooze: true), AlarmClock(alarmName: "알람 2", alarmTime: "12:30", sound: "TOMBOY", isSnooze: true)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         alarmTableView.dataSource = self
+        alarmTableView.delegate = self
 
     }
+    
 }
+   
 
 extension AlarmViewController : UITableViewDataSource {
     
@@ -27,10 +30,37 @@ extension AlarmViewController : UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = alarmTableView.dequeueReusableCell(withIdentifier: "alarmTableCell", for: indexPath)
-        cell.textLabel?.text = tableViewItems[indexPath.row]
+        
+        let alarmTime = cell.viewWithTag(101) as? UILabel
+        let alarmName = cell.viewWithTag(102) as? UILabel
+        //let alarmIsEnabled = cell.viewWithTag(103) as? UISwitch
+        
+        alarmTime?.text = tableViewItems[indexPath.row].alarmTime
+        alarmTime?.textColor = .white
+        alarmName?.text = tableViewItems[indexPath.row].alarmName
+        alarmName?.textColor = .white
+        
+        let bgColorView = UIView()
+        bgColorView.backgroundColor = UIColor(displayP3Red: 40/255, green: 40/255, blue: 40/255, alpha: 1)
+        cell.selectedBackgroundView = bgColorView
+        
+        
         return cell
     }
-    
-
-
+        
 }
+
+extension AlarmViewController : UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
+    
+    
+}
+
+
+
