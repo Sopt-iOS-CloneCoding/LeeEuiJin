20adb0fb57f3229348e4d83298036406332a26c1
lee-euijin <leeuj9663@naver.com>
2022-04-17 14:16:29 +0900

    #1 ✨[FEAT] : 알림화면 레이아웃 구성 (2)  : 테이블 뷰 생성

--- Clock/Clock/Screen/Alarm/AlarmViewController.swift
+++ Clock/Clock/Screen/Alarm/AlarmViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class AlarmViewController: UIViewController {
-
+class AlarmViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var alarmTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        alarmTableView.delegate = self
+        alarmTableView.dataSource = self
 
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.init(style: .default, reuseIdentifier: "myCell")
+        cell.textLabel?.text = "Hello World"
+        return cell
+    }
 
 
 }
